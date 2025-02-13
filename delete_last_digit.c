1cc723e4d411bfe3b72b0aa0f1130596b513ec6d
Kotresh Menasinakai <147266521+Kotreshmenasinakai@users.noreply.github.com>
2025-02-13 11:14:10 +0530

    Add files via upload

---
+++ delete_last_digit.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+
+{
+int i =253,j;
+j=i/10;
+printf("after delete the last digit from %d the number is %d \n", i,j);
+
+
+}
+
+
---
+++ swap.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+
+{
+
+int i =35, j=25, temp;
+temp = j;
+j=i;
+i=temp;
+printf("before i=35 j=25\n after i=%d j=%d \n",i,j);
+
+}
